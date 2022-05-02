dce287ff432def5583dc39c3532fda772b21c9bc
_whyvrafvr <jsie@trendev.fr>
2022-05-02 19:19:15 +0200

    impl. GenerateN() + tests

--- pkg/key.go
+++ pkg/key.go
@@ -10,6 +10,7 @@ import (
 
 var ErrCastingPublicKeyToECDSA = errors.New("error casting public key to ECDSA")
 
+// Generate returns a private key and its public address as two strings.
 func Generate() (prk string, a string, err error) {
 	prkECDSA, err := crypto.GenerateKey()
 	if err != nil {
@@ -25,4 +26,22 @@ func Generate() (prk string, a string, err error) {
 	return
 }
 
-func GenerateN(n int) {}
+// GenerateN returns a map of private keys and public addresses. If N <= 0, generates a single entry.
+func GenerateN(N int) (map[string]string, error) {
+	n := 0
+	if N <= 0 {
+		n = 1
+	} else {
+		n = N
+	}
+
+	m := map[string]string{}
+	for i := 0; i < n; i++ {
+		k, a, err := Generate()
+		if err != nil {
+			return nil, err
+		}
+		m[k] = a
+	}
+	return m, nil
+}
---
+++ pkg/key_test.go
@@ -0,0 +1,57 @@
+package key
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+var (
+	prkRegExp     = regexp.MustCompile(`^[a-f0-9]{64}$`)
+	addressRegExp = regexp.MustCompile(`^0x[a-fA-F0-9]{40}$`)
+)
+
+func TestGenerate(t *testing.T) {
+	k, a, err := Generate()
+	if err != nil {
+		t.Errorf("error generating private key / public address: %v\n", err)
+		t.FailNow()
+	}
+
+	if !prkRegExp.Match([]byte(k)) {
+		t.Errorf("%q is not a private key !\n", k)
+		t.FailNow()
+	}
+
+	if !addressRegExp.Match([]byte(a)) {
+		t.Errorf("%q is not a public address !\n", a)
+		t.FailNow()
+	}
+}
+
+func TestGenerateN(t *testing.T) {
+	tt := []struct {
+		v, exp int
+	}{
+		{-1, 1},
+		{2, 2},
+		{5, 5},
+		{10, 10},
+		{100, 100},
+		{1000, 1000},
+		{0, 1},
+	}
+	for _, tc := range tt {
+		t.Run(fmt.Sprintf("%d", tc.v), func(t *testing.T) {
+			m, err := GenerateN(tc.v)
+			if err != nil {
+				t.Errorf("error generating %d private key / public address: %v\n", tc.v, err)
+				t.FailNow()
+			}
+			if len(m) != tc.exp {
+				t.Errorf("incorrect map length, got %d, want %d\n", len(m), tc.exp)
+				t.FailNow()
+			}
+		})
+	}
+}
