2c32c4edf295df57a09140066c83261c2b632ad3
_whyvrafvr <jsie@trendev.fr>
2022-05-02 18:32:37 +0200

    change color

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, "🔑 private key:\t\u001b[42m%s\u001b[0m\n", prk)
-	fmt.Fprintf(w, "🚩 public address:\t\u001b[0;32m%s\u001b[0m\n", a)
+	fmt.Fprintf(w, "🔑 private key:\t\u001b[1;34m%s\u001b[0m\n", prk)
+	fmt.Fprintf(w, "🚩 public address:\t\u001b[42m%s\u001b[0m\n", a)
 	w.Flush()
 }
